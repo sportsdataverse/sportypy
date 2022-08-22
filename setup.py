46e1c2d2838a9f917ccbd263365a104e77bd7f24
Ross Drucker <ross.a.drucker@gmail.com>
2022-08-21 21:32:48 -0500

    Initial setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="sportypy",
+    version="1.0.0",
+    description="Draw sports surfaces according to their rulebook specifications",
+    long_description=open("README.md").read(),
+    url="https://github.com/rossdrucker/sportypy",
+    author="Ross Drucker",
+    author_email="ross.a.drucker@gmail.com",
+    license="GPL 3.0",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ]
+)
