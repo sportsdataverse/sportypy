1a05792bca3b0799d2c144e0426a0edbd4f63845
Ross Drucker <ross.a.drucker@gmail.com>
2022-09-13 18:04:14 -0500

    Updated package internals

--- setup.py
+++ setup.py
@@ -7,18 +7,48 @@ setuptools.setup(
     long_description = open("README.md").read(),
     long_description_content_type = "text/markdown",
     url = "https://github.com/sportsdataverse/sportypy",
+    project_urls = {
+        "Source": "https://github.com/sportsdataverse/sportypy",
+        "Docs": "https://sportypy.sportsdataverse.org/",
+        "Bug Tracker": "https://github.com/sportsdataverse/sportypy/issues",
+    },
     author = "Ross Drucker",
     author_email = "ross.a.drucker@gmail.com",
+    maintainer = "Ross Drucker",
     license = "GPL 3.0",
-    packages = setuptools.find_packages(),
-    install_requires = [],
+    packages = setuptools.find_packages(exclude = ["tests", "tests.*"]),
+    install_requires = [
+        "numpy",
+        "scipy",
+        "pandas",
+        "matplotlib",
+    ],
+    extras_require = {
+        "test": [
+            "flake8",
+            "pytest",
+            "pydocstyle",
+            "pycodestyle",
+        ],
+        "docs": [
+            "sphinx",
+            "livereload",
+            "myst-parser",
+            "sphinx-markdown-builder",
+            "sphinx-bootstrap-theme",
+        ]
+    },
     classifiers = [
-        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-    ]
+    ],
+    include_package_data = True,
+    package_data = {
+        "sportypy.data": ["*.json"]
+    }
 )
